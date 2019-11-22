1aadd79ffc138b961237305c77014f3610648851
Leonardo Souza Dias <leonnardosouza96@gmail.com>
2019-11-22 01:07:32 -0300

    listagem de produtos

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+// import api from "./services/api";
+import "./styles.css";
+import Main from "./pages/main";
+
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
+);
+
+export default App;
