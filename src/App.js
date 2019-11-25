567be5651842d0d6350f4a8d16f25bccd3ad69e7
Leonardo Souza Dias <leonnardosouza96@gmail.com>
2019-11-25 14:17:46 -0300

    navegacao finalizada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Routes from "./routes";
 // import api from "./services/api";
 import "./styles.css";
 import Main from "./pages/main";
@@ -8,7 +9,7 @@ import Main from "./pages/main";
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
